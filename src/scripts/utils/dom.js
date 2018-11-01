704a84432ca91f14dc93427b8a3cc393bdd46611
Michal Lester <michal.lester@allegro.pl>
2018-11-01 19:04:59 +0100

    add eslint, cleaning

---
+++ src/scripts/utils/dom.js
@@ -0,0 +1,5 @@
+export default class DOM {
+  constructor(document) {
+    this.document = document;
+  }
+}
--- tests/helloWorld.test.js
+++
@@ -1,7 +0,0 @@
-import helloWorld from '../src/scripts/helloWorld'
-
-describe('should display text', function () {
-    it('hello world', function() {
-        expect(helloWorld()).toBe('Hello World');
-    })
-});
