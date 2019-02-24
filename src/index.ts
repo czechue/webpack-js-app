7696ceda1f35f811d43289931f0b71eedd634240
Michal Lester <michal.lester@allegro.pl>
2019-02-24 16:40:29 +0100

    working prompt out with templates question

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-const strange = (text: string): void => console.log(text)
+#!/usr/bin/env node
 
-strange('dog');
+import * as fs from 'fs';
+import * as path from 'path';
+import * as inquirer from 'inquirer';
+import chalk from 'chalk';
+
+const CHOICES = fs.readdirSync(path.join(__dirname, '../templates'));
+
+const QUESTIONS = [
+    {
+        name: 'template',
+        type: 'list',
+        message: 'What project template would you like to generate?',
+        choices: CHOICES
+    },
+    {
+        name: 'name',
+        type: 'input',
+        message: 'Project name:'
+    }];
+
+console.log(CHOICES)
+
+inquirer.prompt(QUESTIONS)
+    .then(answers => {
+        console.log(answers);
+    });
