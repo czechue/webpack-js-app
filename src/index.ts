bddbb6c96d198f1b459085226cbb45d0e3fa1036
Michal Lester <michal.lester@allegro.pl>
2019-02-24 18:49:14 +0100

    copy and transofmr all the content from template to project folder

--- src/index.ts
+++ src/index.ts
@@ -51,13 +51,50 @@ inquirer.prompt(QUESTIONS)
         if (!createProject(targetPath)) {
             return;
         }
+
+        createDirectoryContents(templatePath, projectName)
+
     });
 
-    const createProject = (projectPath: string) => {
-        if (fs.existsSync(projectPath)) {
-            console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
-            return false;
-        }
-        fs.mkdirSync(projectPath);
-        return true;
+const createProject = (projectPath: string) => {
+    if (fs.existsSync(projectPath)) {
+        console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
+        return false;
     }
+    fs.mkdirSync(projectPath);
+    return true;
+}
+
+const SKIP_FILES = ['node_modules', '.template.json'];
+export interface TemplateConfig {
+    files?: string[]
+    postMessage?: string
+}
+const createDirectoryContents = (templatePath: string, projectName: string) => {
+    // read all files/folders (1 level) from template folder
+    const filesToCreate = fs.readdirSync(templatePath);
+
+    // loog each file/folder
+    filesToCreate.forEach(file => {
+        const origFilePath = path.join(templatePath, file);
+
+        // get stats about the current fike
+        const stats = fs.statSync(origFilePath);
+
+        // skip files that should not be copied
+        if (SKIP_FILES.indexOf(file) > -1) return;
+
+        if (stats.isFile()) {
+            // read file content and transform it using template engine
+            let contents = fs.readFileSync(origFilePath, 'utf8');
+            // write file to destination folder
+            const writePath = path.join(CURR_DIR, projectName, file);
+            fs.writeFileSync(writePath, contents, 'utf8');
+        } else if (stats.isDirectory()) {
+            // create folder in destination folder
+            fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+            // copy files/folder inside current folder recursively
+            createDirectoryContents(path.join(templatePath, file), path.join(projectName, file));
+        }
+    })
+}
