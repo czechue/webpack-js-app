9253cb1db65bb485c023c5eeb426b560ab522675
Michal Lester <michal.lester@allegro.pl>
2019-03-03 10:05:24 +0100

    testing with css

--- templates/new-webpack-js-starter/config/webpack.prod.js
+++ templates/new-webpack-js-starter/config/webpack.prod.js
@@ -1,10 +1,11 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common.js');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
+  mode: 'production',
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
@@ -16,10 +17,17 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    // We can use webpack's built in DefinePlugin to define this variable for all our dependencies:
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
   ],
-  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
 });
